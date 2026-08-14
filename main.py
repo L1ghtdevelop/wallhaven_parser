9926d1f0e9105b75b66193bdcba12aa82c5c4a9b
l1ghtdevelop <niirokalostas1234@gmail.com>
2026-08-15 02:24:31 +0300

    refactor, delete nsfw rate, init parse a lot of pages

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ class Parser:
             'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         }
         self.rating = rating
-        self.pages = pages
+        self.to_pages = pages
+        self.num_img = 1
 
     def get_url_rating(self):
         match self.rating.lower():
@@ -20,23 +21,16 @@ class Parser:
                 self.url += "search?categories=111&purity=100&sorting=date_added&order=desc&page=2"
             case "sketchy":
                 self.url += "search?categories=111&purity=010&sorting=date_added&order=desc&page=2"
-            case "nsfw":
-                self.url += "search?categories=111&purity=001&sorting=date_added&order=desc&page=2"
-    
-    def get_link_images(self):
+
+    def get_page_list(self):
         self.get_url_rating()
-        self.number = 1
         response = requests.get(self.url, headers=self.headers, timeout=10)
         response.raise_for_status()
         soup = bs4.BeautifulSoup(response.content, "lxml")
         image_tag = soup.find_all('a')
         resource = self.get_image_page(image_tag)
-        logger.info(resource)
-        for image in resource:
-            logger.info("enter")
-            self.download_image(image)
-            logger.info("exit")
-        
+        self.download_image(resource)
+
     def get_image_page(self, images):
         img_arr = []
         for image_link in images:
@@ -50,18 +44,29 @@ class Parser:
                         img_arr.append(img["src"])
         return img_arr
 
-    def download_image(self, img):
-        h = httplib2.Http('.cache')
-        response, content = h.request(img)
-        logger.info(img)
-        logger.info(self.number)
-        with open(f'src/img{self.number}.jpg', 'wb') as o:
-            self.number += 1
-            o.write(content)
+    def download_image(self, source):
+        for img in source:
+            h = httplib2.Http('.cache')
+            response, content = h.request(img)
+            if self.rating.lower == "sfw":
+                self.open_write_file(content)
+            else:
+                self.open_write_file(content)
+
+    def open_write_file(self, content):
+        out = open(f'src/sfw/img{self.number}.jpg', 'wb')
+        self.number += 1
+        out.write(content)
+        out.close()
+
+    def parse_pages(self):
+        num_page = 2
+        while num_page <= self.to_pages:
+            pass
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename='wallhaven_parser.log', level=logging.INFO)
     parser = Parser("sfw", 10)
-    parser.get_link_images()
 
 
